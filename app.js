59e651d1145937832525d025e6c83f5af4f17a4b
faiz123-maker <66491200+faiz123-maker@users.noreply.github.com>
2020-06-07 04:45:05 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+// Assignment No. 1:
+
+// Chapter # 1:
+
+ window.alert("JavaScript Alert:  Welcome to my website");
+ window.alert("Error! Please enter a valid password");
+ window.alert("JavaScript Alert:   Welcome to JS Land...  Happy Coding");
+ window.alert("JavaScript Alert:   Welcome to JS Land...");
+ window.alert("JavaScript Alert:   Happy Coding! Prevent this page from creating additional dialogs");
+
+// Chapter # 2:
+
+ var username = "FAIZ";
+ var myName = "Syed Faiz Ahmed";
+ var message = "Hello World";
+ window.alert(message);
+
+ var studentname = "John Doe";
+ var studentage = 15;
+ var studentcertificate = "Certified Mobile Application Development"
+ alert(studentname);
+ alert(studentage);
+ alert(studentcertificate);
+
+ var p = "pizza   pizz  piz  pi  pi";
+ alert(p);
+
+ var email = "sfahmed129@gmail.com";
+ alert(email);
+
+ var book = "A smarter way to learn JavaScript";
+ alert(book);
+
+ var firstline = "Yah! I can write HTML content through JavaScript";
+ document.write(firstline);
+
+ var specialchar = "$&*&$";
+ alert(specialchar);
+ document.write(specialchar);
+
+// Chapter # 3:
+
+ var age = 20;
+ alert(age);
+
+ var visittime = "You have visited this site 100 times";
+ alert(visittime);
+
+ var birthYear = 1999;
+ document.write (birthYear);
+
+ var visitorname = "John Doe";
+ var producttitle = "T-Shit";
+ var quantity = 5;
+ document.write(visitorname + " ordered " + quantity + producttitle + " from XYZ Store");
+
+// Chapter # 4:
+
+var Faiz, Sajjad, Aqil = "name";
+
+var name = "Faiz";
+var age = 20;
+var email = "sfahmed129@gmailcom";
+var weight = "60 Kg";
+var height = "5.7 feet";
+
+//Due to reason I comment this particular part of code.
+
+// var ?name = "Faiz";
+// var ?age = 20;
+// var ?email = "sfahmed129@gmailcom";
+// var ?weight = "60 Kg";
+// var ?height = "5.7 feet";
+
+var rules = "Rules for naming JS Variables 1. Variable names can only contain a. Dollar sign b. alphabets c.numerics after alphabets d. underscore For example $my_firstvariable 2. Variable must begins with a a. underscore b. dollar sign c. alphabet 3. Variable names are case sensitive. 4. Variable names should not be JS Keywords." ;
+document.write(rules);
+
+
